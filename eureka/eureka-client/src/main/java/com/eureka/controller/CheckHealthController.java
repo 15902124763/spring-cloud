ceea0127a26bf1715004f5b5720ec8e218d3c915
yarm <15902124763@163.com>
2020-07-10 11:22:15 +0800

    add code

--- eureka/eureka-client/src/main/java/com/eureka/controller/CheckHealthController.java
+++ eureka/eureka-client/src/main/java/com/eureka/controller/CheckHealthController.java
@@ -1,4 +1,12 @@
 package com.eureka.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CheckHealthController {
+    @RequestMapping("/")
+    public String index(){
+        return "it work";
+    }
 }
